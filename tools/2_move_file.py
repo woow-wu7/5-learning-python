9b3cc16cc89456cf32bc999f2ea1800f6eb25f3f
woow-wu7 <woow.wu7@gmail.com>
2024-09-22 00:08:46 +0800

    feat: move files2.

--- tools/2_move_file.py
+++ tools/2_move_file.py
@@ -1,13 +1,13 @@
 import os
 import shutil
 
-path = './files'
+path = './files/'
 files = os.listdir(path)
 
 for f in files:
-    folder_name = './files/' + f.split('.')[-1]
-    if not os.path.exists(folder_name):
-        os.makedirs(folder_name)
-        shutil.move(f, folder_name)
-    else:
-        shutil.move(f, folder_name)
+    file_path = os.path.join(path, f)  # 文件的完整路径
+    if os.path.isfile(file_path):  # 确保只对文件操作，排除目录
+        folder_name = os.path.join(path, f.split('.')[-1])  # 目标文件夹路径
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)  # 创建文件夹
+        shutil.move(file_path, folder_name)  # 移动文件
